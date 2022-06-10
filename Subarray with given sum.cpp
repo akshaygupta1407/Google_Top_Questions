5ce0375290477d4fed4f38b740d68a3b6ed423b4
akshaygupta1407 <85879402+akshaygupta1407@users.noreply.github.com>
2022-06-10 13:49:30 +0530

    Update Subarray with given sum.cpp

--- Subarray with given sum.cpp
+++ Subarray with given sum.cpp
@@ -35,3 +35,25 @@ class Solution
         return {-1};
     }
 };
+
+
+//Subarray Sum Equals K
+/*
+https://leetcode.com/problems/subarray-sum-equals-k/
+*/
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
+        int n = nums.size();
+        int cumsum=0,count=0;
+        mpp[0]++;
+        for(int i=0;i<n;i++)
+        {
+            cumsum+=nums[i];
+            count+=mpp[cumsum-k];
+            mpp[cumsum]++;
+        }
+        return count;
+    }
+};
